d984c407bb765960cf0cc2a0be9c23e8e02a84d5
Sam <anothercat1259@gmail.com>
2021-05-12 14:58:35 +1200

    Add scrolling screen and change some assets

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -20,6 +20,8 @@ SCREEN_TITLE = "Sprite Rooms Example"
 
 MOVEMENT_SPEED = 5
 
+VIEWPORT_MARGIN = 40
+
 
 class Room:
     """
@@ -53,7 +55,7 @@ def setup_room_1():
     for y in (0, SCREEN_HEIGHT - SPRITE_SIZE):
         # Loop for each box going across
         for x in range(0, SCREEN_WIDTH, SPRITE_SIZE):
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            wall = arcade.Sprite("brickWall.png", SPRITE_SCALING *1.7)
             wall.left = x
             wall.bottom = y
             room.wall_list.append(wall)
@@ -64,12 +66,12 @@ def setup_room_1():
         for y in range(SPRITE_SIZE, SCREEN_HEIGHT - SPRITE_SIZE, SPRITE_SIZE):
             # Skip making a block 4 and 5 blocks up on the right side
             if (y != SPRITE_SIZE * 4 and y != SPRITE_SIZE * 5) or x == 0:
-                wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+                wall = arcade.Sprite("brickWall.png", SPRITE_SCALING *1.7)
                 wall.left = x
                 wall.bottom = y
                 room.wall_list.append(wall)
 
-    wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+    wall = arcade.Sprite("brickWall.png", SPRITE_SCALING *1.7)
     wall.left = 7 * SPRITE_SIZE
     wall.bottom = 5 * SPRITE_SIZE
     room.wall_list.append(wall)
@@ -91,20 +93,21 @@ def setup_room_2():
     """ Set up the game and initialize the variables. """
     # Sprite lists
     room.wall_list = arcade.SpriteList()
+    room_width = SCREEN_WIDTH*2
 
     # -- Set up the walls
     # Create bottom and top row of boxes
     # This y loops a list of two, the coordinate 0, and just under the top of window
     for y in (0, SCREEN_HEIGHT - SPRITE_SIZE):
         # Loop for each box going across
-        for x in range(0, SCREEN_WIDTH, SPRITE_SIZE):
+        for x in range(0, room_width, SPRITE_SIZE):
             wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
             wall.left = x
             wall.bottom = y
             room.wall_list.append(wall)
 
     # Create left and right column of boxes
-    for x in (0, SCREEN_WIDTH - SPRITE_SIZE):
+    for x in (0, room_width - SPRITE_SIZE):
         # Loop for each box going across
         for y in range(SPRITE_SIZE, SCREEN_HEIGHT - SPRITE_SIZE, SPRITE_SIZE):
             # Skip making a block 4 and 5 blocks up
@@ -148,10 +151,14 @@ class MyGame(arcade.Window):
         self.player_list = None
         self.physics_engine = None
 
+        # Scrolling
+        self.view_bottom = 0
+        self.view_left = 0
+
     def setup(self):
         """ Set up the game and initialize the variables. """
         # Set up the player
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png", SPRITE_SCALING)
+        self.player_sprite = arcade.Sprite("player_stand.png", SPRITE_SCALING)
         self.player_sprite.center_x = 100
         self.player_sprite.center_y = 100
         self.player_list = arcade.SpriteList()
@@ -174,6 +181,11 @@ class MyGame(arcade.Window):
         # Create a physics engine for this room
         self.setup_physics()
 
+        # Set the viewport boundaries
+        # These numbers set where we have 'scrolled' to.
+        self.view_left = 0
+        self.view_bottom = 0
+
     def setup_physics(self):
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.rooms[self.current_room].wall_list)
 
@@ -186,7 +198,7 @@ class MyGame(arcade.Window):
         arcade.start_render()
 
         # Draw the background texture
-        arcade.draw_lrwh_rectangle_textured(0, 0,
+        arcade.draw_lrwh_rectangle_textured(self.view_left, self.view_bottom,
                                             SCREEN_WIDTH, SCREEN_HEIGHT,
                                             self.rooms[self.current_room].background)
 
@@ -225,6 +237,67 @@ class MyGame(arcade.Window):
         # example though.)
         self.physics_engine.update()
 
+# ----Changing Rooms------
+
+        # Do some logic here to figure out what room we are in, and if we need to go
+        # to a different room.
+        screen_width = SCREEN_WIDTH + self.view_left
+        if self.player_sprite.center_x > screen_width - self.player_sprite.width / 2 and self.current_room == 0:
+            self.current_room = 1
+            self.setup_physics()
+            self.player_sprite.center_x = 0 + self.player_sprite.width / 2
+        elif self.player_sprite.center_x < 0 + self.player_sprite.width / 2 and self.current_room == 1:
+            self.current_room = 0
+            self.setup_physics()
+            self.player_sprite.center_x = screen_width - self.player_sprite.width / 2
+
+        else:
+            # --- Manage Scrolling ---
+
+            # Keep track of if we changed the boundary. We don't want to call the
+            # set_viewport command if we didn't change the view port.
+            changed = False
+
+            # Scroll left
+            left_boundary = self.view_left + VIEWPORT_MARGIN
+            if self.player_sprite.left < left_boundary:
+                self.view_left -= left_boundary - self.player_sprite.left
+                changed = True
+
+            # Scroll right
+            right_boundary = self.view_left + SCREEN_WIDTH - VIEWPORT_MARGIN
+            if self.player_sprite.right > right_boundary:
+                self.view_left += self.player_sprite.right - right_boundary
+                changed = True
+
+            # Scroll up
+            top_boundary = self.view_bottom + SCREEN_HEIGHT - VIEWPORT_MARGIN
+            if self.player_sprite.top > top_boundary:
+                self.view_bottom += self.player_sprite.top - top_boundary
+                changed = True
+
+            # Scroll down
+            bottom_boundary = self.view_bottom + VIEWPORT_MARGIN
+            if self.player_sprite.bottom < bottom_boundary:
+                self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+                changed = True
+
+            # Make sure our boundaries are integer values. While the view port does
+            # support floating point numbers, for this application we want every pixel
+            # in the view port to map directly onto a pixel on the screen. We don't want
+            # any rounding errors.
+            self.view_left = int(self.view_left)
+            self.view_bottom = int(self.view_bottom)
+
+            # If we changed the boundary values, update the view port to match
+            if changed:
+                arcade.set_viewport(self.view_left,
+                                    SCREEN_WIDTH + self.view_left,
+                                    self.view_bottom,
+                                    SCREEN_HEIGHT + self.view_bottom)
+
+        # ----Changing Rooms------
+
         # Do some logic here to figure out what room we are in, and if we need to go
         # to a different room.
         if self.player_sprite.center_x > SCREEN_WIDTH - self.player_sprite.width / 2 and self.current_room == 0:
@@ -235,6 +308,8 @@ class MyGame(arcade.Window):
             self.current_room = 0
             self.setup_physics()
             self.player_sprite.center_x = SCREEN_WIDTH - self.player_sprite.width / 2
+
+
 
 
 def main():
